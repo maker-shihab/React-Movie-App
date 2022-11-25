20d3860b266ee045d2a01fc1e03f08b0d162ee27
Yashwant1849 <frontendmaker99@gmail.com>
2022-11-25 09:09:11 +0600

    Update files

---
+++ movie-app/movie-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Headers from './Components/Headers/Headers';
+
+function App() {
+  return (
+    <>
+      <Headers></Headers>
+    </>
+  );
+}
+
+export default App;
---
+++ movie-app/movie-app/src/Components/Headers/Headers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Headers.css';
+
+const Headers = () => {
+    return (
+        <header>
+            <div className='header-content'>
+                <h1>🎬 ENTERTAINMENT HUB 🎥</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Headers;
